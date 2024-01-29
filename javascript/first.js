50f9adde162c0ee5d50efc009c0ffe11b2b12ee3
Salonijain2210 <salonij2210@gmail.com>
2024-01-29 09:30:34 -0800

    first commit

---
+++ javascript/first.js
@@ -0,0 +1,18 @@
+alert("hello your webpage works")
+
+
+setTimeout (function(){
+    alert ("I am inside set timeout")
+ },2000)
+
+let c = document.getElementById("slider1");
+c.style.color = "red";
+
+let ctitles = document.querySelector(".card-title");
+console.log(ctitles);
+
+document.querySelector (".cus-btn") . style.color="black"
+let s= document.ATTRIBUTE_NODE("span")[0]
+console.log(s); 
+
+
